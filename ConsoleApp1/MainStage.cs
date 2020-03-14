b3db2aa041796619bcd9f1eaddaa219cab53f174
Ryan.Martin <Ryan.Martin@VUHL-C2C01Z2>
2020-03-14 01:58:57 -0500

    forgot to save after uncommenting :)

--- ConsoleApp1/MainStage.cs
+++ ConsoleApp1/MainStage.cs
@@ -16,13 +16,13 @@ namespace ConsoleApp1
             Console.ForegroundColor = ConsoleColor.White;
             Random rand = new Random();
             bool exit = false;
-            //Logo.ThemeSong();
+            Logo.ThemeSong();
             string specialName = Speciality.GetSpeciality();
             Speciality boogerSpecial = new Speciality(specialName, Speciality.GetValue(specialName));
             Booger booger = new Booger(DragNames.GetBoogerDragName(), rand.Next(0, 21), rand.Next(5, 21),
                 rand.Next(60, 301), rand.Next(21, 40), boogerSpecial, rand.Next(10, 31));
-            //Console.WriteLine("You Walk into the WerkRoom, What is Your Real Name?");
-            //string playerName = Console.ReadLine();
+            Console.WriteLine("You Walk into the WerkRoom, What is Your Real Name?");
+            string playerName = Console.ReadLine();
             Console.WriteLine("Welcome!\nYour Drag Name is: {0}\n", booger.Name);
             do
             {
@@ -33,33 +33,33 @@ namespace ConsoleApp1
                     heatherTwo = Heather.GenerateHeather();
                 }
                 Song singleSong = new Song(Song.SelectASong());
-                //Console.WriteLine($"You are in the bottom 3 with {heather.Name} and {heatherTwo.Name}");
-                //for (int i = 0; i < 50; i++)
-                //{
-                //    Console.Write('-');
-                //    Thread.Sleep(75);
-                //}
+                Console.WriteLine($"You are in the bottom 3 with {heather.Name} and {heatherTwo.Name}");
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.Write('-');
+                    Thread.Sleep(75);
+                }
                 int draw = new Random().Next(0, 2);
                 if (draw == 0)
                 {
                     heather = heatherTwo;
                 }
-                //Console.WriteLine($"\nRuPaul: {booger.Name}, {heather.Name}...  I'm sorry my dears but you are both up for elimination"); Thread.Sleep(4000);
-                //Console.WriteLine($"\tEach of you has been asked to prepare a lip sync to \"{singleSong.Name}\" by {singleSong.Artist}"); Thread.Sleep(4000);
-                //Console.WriteLine("\tTwo queens stand before me, ladies this is your last chance to impress me and save yourself from elimination."); Thread.Sleep(4000);
-                //Console.Write("\tThe time has come for you to lipsync");
-                //for (int i = 0; i < 3; i++)
-                //{
-                //    Console.Write('.');
-                //    Thread.Sleep(900);
-                //}
-                //Console.Write("FOR"); Thread.Sleep(1000);
-                //Console.Write(" YOUR"); Thread.Sleep(1000);
-                //Console.WriteLine(" LIFE!"); Thread.Sleep(1000);
+                Console.WriteLine($"\nRuPaul: {booger.Name}, {heather.Name}...  I'm sorry my dears but you are both up for elimination"); Thread.Sleep(4000);
+                Console.WriteLine($"\tEach of you has been asked to prepare a lip sync to \"{singleSong.Name}\" by {singleSong.Artist}"); Thread.Sleep(4000);
+                Console.WriteLine("\tTwo queens stand before me, ladies this is your last chance to impress me and save yourself from elimination."); Thread.Sleep(4000);
+                Console.Write("\tThe time has come for you to lipsync");
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write('.');
+                    Thread.Sleep(900);
+                }
+                Console.Write("FOR"); Thread.Sleep(1000);
+                Console.Write(" YOUR"); Thread.Sleep(1000);
+                Console.WriteLine(" LIFE!"); Thread.Sleep(1000);
                 //Lightning.LightningFlashes();
-                //Console.Write("RuPaul: Good Luck"); Thread.Sleep(1000);
-                //Console.WriteLine(" and don't *&#! it up!"); Thread.Sleep(2000);
-                //Song.PlaySong(singleSong);
+                Console.Write("RuPaul: Good Luck"); Thread.Sleep(1000);
+                Console.WriteLine(" and don't *&#! it up!"); Thread.Sleep(2000);
+                Song.PlaySong(singleSong);
                 bool reload = false;
                 do
                 {
